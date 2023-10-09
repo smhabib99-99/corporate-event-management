7e8b8bc686febc283de768210c4e242f612e63a2
smhabib99-99 <habibaust34@gmail.com>
2023-10-09 06:10:37 +0600

    json data fetch and title all functionalities shown in home page

--- src/pages/Service/ServiceCard.jsx
+++ src/pages/Service/ServiceCard.jsx
@@ -17,7 +17,7 @@ const ServiceCard = ({servicee}) => {
                 </div>
                 <div className="p-6">
                     <div className="mb-2 flex items-center justify-between">
-                        <p className="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
+                        <p className="block font-sans text-2xl font-semibold bg-sky-400 text-white rounded-md px-3 leading-relaxed text-blue-gray-900 antialiased">
                             {/* Apple AirPods */}
                             {title}
                         </p>
@@ -26,7 +26,7 @@ const ServiceCard = ({servicee}) => {
                             {price}
                         </p>
                     </div>
-                    <p className="block font-sans text-sm font-normal leading-normal text-gray-700 antialiased opacity-75">
+                    <p className="block font-sans text-base font-medium leading-normal text-gray-700 antialiased opacity-75">
                         {/* With plenty of talk and listen time, voice-activated Siri access, and an */}
                         {/* available wireless charging case. */}
                         {description}
@@ -34,10 +34,10 @@ const ServiceCard = ({servicee}) => {
                 </div>
                 <div className="p-6 pt-0">
                     <button
-                        className="block w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                        className="block mx-auto bg-amber-400 text-indigo-600 select-none rounded-lg bg-blue-gray-900/10 py-3 px-6  align-middle font-sans text-2xl text-center font-bold text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                         type="button"
                     >
-                        Add to Cart
+                        Show Details
                     </button>
                 </div>
             </div>
